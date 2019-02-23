d35259dbee9961d95c26d149c9a4ca3e80b531dd
EmperorlNeo <neomartindelcampo2001@gmail.com>
2019-02-23 11:32:38 -0800

    Nasty Suprise
    finished

--- src/NastySurprise.java
+++ src/NastySurprise.java
@@ -3,6 +3,8 @@ import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -13,44 +15,64 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class NastySurprise {
-public static void main(String[] args) {
+public class NastySurprise implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton button = new JButton();
 	JButton button2 = new JButton();
-	frame.setVisible(true);
-	frame.add(panel);
-	panel.add(button);
-	panel.add(button2);
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.setBackground(Color.BLACK);
-	panel.setBackground(Color.BLACK);
-	frame.setTitle("OR");
-	panel.add(button);
-	panel.add(button2);
-	button.setText("Trick");
-	button2.setText("Treat");
-	button.setPreferredSize(new Dimension(950, 1200));
-	button2.setPreferredSize(new Dimension(950, 1200));
-	button.setVisible(true);
-	frame.setSize(2100, 1800);
-	frame.pack();
 
-	showPictureFromTheInternet("https://cdn.akc.org/Marketplace/Breeds/Siberian_Husky_SERP.jpg");
-}
+	public static void main(String[] args) {
+		new NastySurprise().createUI();
+
+	}
+
 	private static void showPictureFromTheInternet(String imageUrl) {
-	     try {
-	          URL url = new URL(imageUrl);
-	          Icon icon = new ImageIcon(url);
-	          JLabel imageLabel = new JLabel(icon);
-	          JFrame frame = new JFrame();
-	          frame.add(imageLabel);
-	          frame.setVisible(true);
-	          frame.pack();
-	     } catch (MalformedURLException e) {
-	          e.printStackTrace();
-	     }
-}
+		try {
+			URL url = new URL(imageUrl);
+			Icon icon = new ImageIcon(url);
+			JLabel imageLabel = new JLabel(icon);
+			JFrame frame = new JFrame();
+			frame.add(imageLabel);
+			frame.setVisible(true);
+			frame.pack();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton buttonPressed = (JButton) e.getSource();
+		if (buttonPressed == button) {
+			showPictureFromTheInternet("https://cdn.akc.org/Marketplace/Breeds/Siberian_Husky_SERP.jpg");
+		}
+		if (buttonPressed == button2) {
+			showPictureFromTheInternet(
+					"https://i.pinimg.com/236x/18/77/97/1877975c291e712e9567692d80f664c3--creepy-dolls-creepy-art.jpg?b=t");
+		}
+	}
+
+	void createUI() {
+		frame.setVisible(true);
+		frame.add(panel);
+		panel.add(button);
+		panel.add(button2);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBackground(Color.BLACK);
+		panel.setBackground(Color.BLACK);
+		frame.setTitle("OR");
+		panel.add(button);
+		panel.add(button2);
+		button.setText("Trick");
+		button2.setText("Treat");
+		button.setPreferredSize(new Dimension(950, 1200));
+		button2.setPreferredSize(new Dimension(950, 1200));
+		button.setVisible(true);
+		frame.setSize(2100, 1800);
+		frame.pack();
+		button.addActionListener(this);
+		button2.addActionListener(this);
+	}
 }
 //if statements and action listeners so that you could put images with the buttons
