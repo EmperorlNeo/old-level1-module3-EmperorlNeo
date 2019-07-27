1374c8d7699c79aa41acaea6462ba9cd56edb9a7
EmperorlNeo <league@WTS-MM-7.local>
2019-07-27 11:28:10 -0700

    Slot Machine
    Need to load images

--- src/SlotMachine.java
+++ src/SlotMachine.java
@@ -1,17 +1,26 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URL;
 import java.util.Random;
-
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class SlotMachine {
+public class SlotMachine implements ActionListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	Random rand = new Random();
 	JButton button = new JButton();
+	JLabel label = new JLabel();
 	int r;
 	int s;
 	int w;
+	int win;
+
 	public static void main(String[] args) {
 		SlotMachine slot = new SlotMachine();
 	}
@@ -26,16 +35,94 @@ public class SlotMachine {
 		frame.add(panel);
 		frame.setTitle("SLOT MACHINE");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(1536,600);
+		frame.setSize(1536, 600);
+		frame.setLocation(300, 250);
 		panel.add(button);
 		button.setText("SPIN");
+		button.addActionListener(this);
 	}
+
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("pressed " + ((JButton) e.getSource()).getText() + " button");
+		JButton buttonPressed = (JButton) e.getSource();
+		if (button == buttonPressed) {
+			showImages();
+		}
+	}
+
 	public void showImages() {
 		int r = rand.nextInt(5);
 		System.out.println(r);
+		if (r == 0) {
+			loadImageFromComputer("7.png");
+
+		}
+		if (r == 1) {
+			loadImageFromComputer("Bell.png");
+		}
+		if (r == 2) {
+			loadImageFromComputer("Cherry.png");
+		}
+		if (r == 3) {
+			loadImageFromComputer("Grapes.png");
+		}
+		if (r == 4) {
+			loadImageFromComputer("Lemon.png");
+		}
+		if (r == 5) {
+			loadImageFromComputer("Orange.png");
+		}
 		int s = rand.nextInt(5);
 		System.out.println(s);
+		if (s == 0) {
+			loadImageFromComputer("7.png");
+		}
+		if (s == 1) {
+			loadImageFromComputer("Bell.png");
+		}
+		if (s == 2) {
+			loadImageFromComputer("Cherry.png");
+		}
+		if (s == 3) {
+			loadImageFromComputer("Grapes.png");
+		}
+		if (s == 4) {
+			loadImageFromComputer("Lemon.png");
+		}
+		if (s == 5) {
+			loadImageFromComputer("Orange.png");
+		}
 		int w = rand.nextInt(5);
 		System.out.println(w);
+		if (w == 0) {
+			loadImageFromComputer("7.png");
+		}
+		if (w == 1) {
+			loadImageFromComputer("Bell.png");
+		}
+		if (w == 2) {
+			loadImageFromComputer("Cherry.png");
+		}
+		if (w == 3) {
+			loadImageFromComputer("Grapes.png");
+		}
+		if (w == 4) {
+			loadImageFromComputer("Lemon.png");
+		}
+		if (w == 5) {
+			loadImageFromComputer("Orange.png");
+		}
+		if(r==s && s==w) {
+			JOptionPane.showMessageDialog(null, "YOU WIN");
+			win=+1;
+			System.out.println(win);
+		}
+	}
+	
+	public JLabel loadImageFromComputer(String fileName) {
+		URL imageURL = getClass().getResource(fileName);
+		Icon icon = new ImageIcon(imageURL);
+		return new JLabel(icon);
 	}
 }
